53668b278655d2cc4fe9311518c098852e2938be
Andrey Gagarin <andreygagarinchel@gmail.com>
2021-12-28 15:36:25 +0500

    New activity for game proccess

---
+++ app/src/main/java/com/twister_ray/quiz/GameActivity.java
@@ -0,0 +1,54 @@
+package com.twister_ray.quiz;
+
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.squareup.picasso.Picasso;
+import java.util.List;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class GameActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_game);
+    final TextView textView = findViewById(R.id.textView);
+    final Button button1 = findViewById(R.id.button1);
+    final Button button2 = findViewById(R.id.button2);
+    final Button button3 = findViewById(R.id.button3);
+    final Button button4 = findViewById(R.id.button4);
+    final ImageView imageView = findViewById(R.id.imageView);
+    final String BASE_URL = "https://quiz.andreygagarin.buzz";
+
+    NetworkService.getInstance().getJsonApi().getAllQuiz().enqueue(new Callback<List<QuizJson>>() {
+      @Override
+      public void onResponse(@NonNull Call<List<QuizJson>> call,@NonNull Response<List<QuizJson>> response) {
+        List<QuizJson> quizzes = response.body();
+        for (QuizJson quiz : quizzes) {
+          String url = BASE_URL + quiz.getImage();
+          textView.setText(quiz.getQuestion().getDescription());
+          List<AnswerJson> answers = quiz.getAnswerJson();
+          button1.setText(answers.get(0).getDescription());
+          button2.setText(answers.get(1).getDescription());
+          button3.setText(answers.get(2).getDescription());
+          button4.setText(answers.get(3).getDescription());
+          Picasso.get()
+              .load(url)
+              .into(imageView);
+        }
+      }
+
+      @Override
+      public void onFailure(@NonNull Call<List<QuizJson>> call, @NonNull Throwable t) {
+        textView.append("Error occurred while getting request!");
+        t.printStackTrace();
+      }
+    });
+  }
+}
